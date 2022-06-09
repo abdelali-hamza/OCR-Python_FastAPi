4663bda46e4fa0685f8d68c7d905402ecad2b1b7
Hamza Abdelali <ka_hamza@esi.dz>
2022-06-09 14:23:10 +0100

    finished

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI , UploadFile , File
+import OCR
+from PIL import Image
+import io
+
+
+app = FastAPI()
+
+@app.post("/predict")
+async def root(file: UploadFile = File(...)):
+  image =  await file.read()
+  image = Image.open(io.BytesIO(image))
+  return {"result":OCR.resultat_final(image)}
+
+@app.get("/corr")
+async def ocr(img_path : str):
+     test = Image.open(img_path)
+     chiffre , score , font = OCR.resultat_final(test)
+     return  {"result":chiffre,"score":score,"font":font}
+
+
+
+
+
